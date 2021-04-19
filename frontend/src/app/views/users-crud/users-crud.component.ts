68448f2ec7c716fa038e73824f04f18e093a564e
RafaelNC98 <rafaelnadalinicarvalho@gmail.com>
2021-04-19 14:02:00 -0300

    finalizando..

--- frontend/src/app/views/users-crud/users-crud.component.ts
+++ frontend/src/app/views/users-crud/users-crud.component.ts
@@ -1,3 +1,5 @@
+import { HeaderService } from './../../components/template/header/header.service';
+import { HeaderData } from './../../components/template/header/header-data.model';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
 
@@ -8,8 +10,13 @@ import { Router } from '@angular/router'
 })
 export class UsersCrudComponent implements OnInit {
 
-  constructor(private router: Router) { }
-
+  constructor(private router: Router, headerService: HeaderService){  
+    headerService.headerData = {
+      title: 'Cadastro de Usuários',
+      icon: 'person',
+      routerUrl: '/users'
+    }
+  }
   ngOnInit(): void {
   }
 
